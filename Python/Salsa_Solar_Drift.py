de4c542fc58115e02982f2ff115f51d292930ddd
sakhawat18 <s.hossain18@gmail.com>
2017-04-12 14:01:49 +0600

    Added a change.

--- Python/Salsa_Solar_Drift.py
+++ Python/Salsa_Solar_Drift.py
@@ -7,7 +7,7 @@ print(data)
 data.shape
 data = data[0,0]
 plt.plot(data)
-plt.xlabel("Time")
+plt.xlabel("VLSR")
 plt.ylabel("Antenna Temperature(K)")
 plt.title("Solar Drift Scan Plot")
 plt.grid(True)
